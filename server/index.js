26ee94a16e1ba14c653db045e77186f8ed8eb373
eishal4a <eishal.cecos@gmail.com>
2026-02-07 00:24:35 -0800

    save work before pull

--- server/index.js
+++ server/index.js
@@ -14,7 +14,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
