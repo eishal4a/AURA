a17f300b503c90f07572a74503d8854bb4577b7d
eishal4a <eishal.cecos@gmail.com>
2026-02-08 05:46:53 -0800

    save work before pull

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://continuing-melba-aurasher-ea9ffc65.koyeb.app/api",
+    baseURL: "https://continuing-melba-aurasher-ea9ffc65.koyeb.app/",
 });
 
 API.interceptors.request.use((req) => {
